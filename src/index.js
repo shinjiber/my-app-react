baea82bcdd1dc849ba0a7446041b8d8fa4312b95
shinjiber <shinjiber7@gmail.com>
2019-05-17 10:20:26 +0900

    array object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Switch from './App';
+// import Switch from './App';
 
-
+const animals = ['cat', 'dog', 'mouse']
 ReactDOM.render(
-  <Switch  />,
+  <div>
+    <ul>
+      <li>{animals[0]}</li>
+      <li>{animals[1]}</li>
+      <li>{animals[2]}</li>
+    </ul>
+  </div>,
   document.getElementById('root')
 );
